b7261b926eed333be8d4e913cfa4bf4652f1791b
Esteban <esteban.quirozv@gmail.com>
2022-10-29 17:24:27 -0500

    advances on AgregarComentario

--- src/main/java/com/alkomprar/tasks/AgregarComentario.java
+++ src/main/java/com/alkomprar/tasks/AgregarComentario.java
@@ -3,17 +3,13 @@ package com.alkomprar.tasks;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.DoubleClick;
-import net.serenitybdd.screenplay.actions.Hit;
-import net.serenitybdd.screenplay.actions.Scroll;
+import net.serenitybdd.screenplay.actions.*;
 import net.serenitybdd.screenplay.actions.type.Type;
 import net.serenitybdd.screenplay.waits.WaitUntil;
 import org.openqa.selenium.Keys;
 
 import static com.alkomprar.userinterfaces.ConfirmacionCarritoPage.CERRAR_MODAL;
-import static com.alkomprar.userinterfaces.DetalleArticuloPage.AGREGAR_ELEMENTO_CARRITO;
-import static com.alkomprar.userinterfaces.DetalleArticuloPage.DIV_PRIMER_ELEMENTO;
+import static com.alkomprar.userinterfaces.DetalleArticuloPage.*;
 import static com.alkomprar.userinterfaces.MenuPage.TXT_BUSCAR;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isClickable;
@@ -71,7 +67,9 @@ public class AgregarComentario implements Task {
         actor.attemptsTo(
                 Type.theValue(producto).into(TXT_BUSCAR),
                 DoubleClick.on(TXT_BUSCAR),
-                Hit.the(Keys.ENTER).into(TXT_BUSCAR)
+                Hit.the(Keys.ENTER).into(TXT_BUSCAR),
+                WaitUntil.the(SELECCIONAR_PRIMER_ELEMENTO, isClickable()),
+                Click.on(SELECCIONAR_PRIMER_ELEMENTO)
         );
     }
 }
--- src/main/java/com/alkomprar/userinterfaces/DetalleArticuloPage.java
+++ src/main/java/com/alkomprar/userinterfaces/DetalleArticuloPage.java
@@ -6,4 +6,5 @@ import org.openqa.selenium.By;
 public class DetalleArticuloPage {
 	public static final Target DIV_PRIMER_ELEMENTO = Target.the("Primer elemento").locatedByFirstMatching("//h2[@class='product__information--name']");
 	public static final Target AGREGAR_ELEMENTO_CARRITO = Target.the("Agregar elemento al carrito").locatedByFirstMatching("//button[contains(text(), 'Agregar al carrito')]");
+	public static final Target SELECCIONAR_PRIMER_ELEMENTO = Target.the("Seleccionar primer elemento").locatedByFirstMatching("//div[@class='product__item__information__image']/a[@class='js-algolia-product-click']");
 }
